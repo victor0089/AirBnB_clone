0d6389cd38149a4c7d154d832b251da02e85d260
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-10-14 12:39:39 +0300

    Update test_base_model.py

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from models.base_model import BaseModel
 
 my_model = BaseModel()
-my_model.name = "My First Model"
+my_model.name = 'My First Model'
 my_model.my_number = 89
-print(my_model)
+print my_model
 my_model.save()
-print(my_model)
+print my_model
 my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
+print my_model_json
+print 'JSON of my_model:'
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    print '\t{}: ({}) - {}'.format(key, type(my_model_json[key]),
+                                   my_model_json[key])
